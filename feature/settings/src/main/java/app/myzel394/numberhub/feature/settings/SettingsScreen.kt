a8a5e36047cc083eea94d2f47707ed1988912ef8
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-04-26 14:40:34 +0200

    feat: Add announcement dialog

--- feature/settings/src/main/java/app/myzel394/numberhub/feature/settings/SettingsScreen.kt
+++ feature/settings/src/main/java/app/myzel394/numberhub/feature/settings/SettingsScreen.kt
@@ -52,6 +52,7 @@ import androidx.compose.material.icons.filled.Vibration
 import androidx.compose.material.icons.filled._123
 import androidx.compose.material.icons.outlined.CheckCircle
 import androidx.compose.material.icons.outlined.NewReleases
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -59,12 +60,14 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -200,6 +203,30 @@ private fun SettingsScreen(
                 .verticalScroll(rememberScrollState())
                 .padding(padding),
         ) {
+            var showNewAppInfo by rememberSaveable {
+                mutableStateOf(false)
+            }
+
+            if (showNewAppInfo) {
+                AlertDialog(
+                    icon = {
+                        Icon(Icons.Outlined.CheckCircle, null)
+                    },
+                    title = {
+                        Text(stringResource(R.string.settings_numberhub_newApp))
+                    },
+                    text = {
+                        Text(stringResource(R.string.settings_numberhub_newApp_announcement))
+                    },
+                    confirmButton = {
+                        TextButton(onClick = { showNewAppInfo = false }) {
+                            Text(stringResource(R.string.ok_label))
+                        }
+                    },
+                    onDismissRequest = { showNewAppInfo = false },
+                )
+            }
+
             AnimatedVisibility(
                 visible = !uiState.hasSeenNewAppAnnouncement,
                 enter = expandVertically() + fadeIn(),
@@ -215,6 +242,7 @@ private fun SettingsScreen(
                     title = title,
                     support = stringResource(R.string.settings_numberhub_newApp_message),
                 ) {
+                    showNewAppInfo = true
                     //onHasSeenNewAppAnnouncement(true)
                 }
             }
