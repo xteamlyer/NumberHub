f3e31da67c6c96b9f1c0c671c4858576d701aa48
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-05-15 20:01:44 +0200

    fix: Undo unwanted changes

--- feature/settings/src/main/java/app/myzel394/numberhub/feature/settings/about/AboutScreen.kt
+++ feature/settings/src/main/java/app/myzel394/numberhub/feature/settings/about/AboutScreen.kt
@@ -85,6 +85,34 @@ private fun AboutScreen(
                 )
             }
 
+            // TERMS AND CONDITIONS
+            // item {
+            //     ListItem(
+            //         icon = Icons.Default.PrivacyTip,
+            //         headlineText = stringResource(R.string.settings_terms_and_conditions),
+            //         modifier = Modifier.clickable {
+            //             openLink(
+            //                 mContext,
+            //                 "https://sadellie.github.io/unitto/terms",
+            //             )
+            //         },
+            //     )
+            // }
+
+            // PRIVACY POLICY
+            item {
+                ListItem(
+                    icon = Icons.Default.Policy,
+                    headlineText = stringResource(R.string.settings_privacy_policy),
+                    modifier = Modifier.clickable {
+                        openLink(
+                            mContext,
+                            "https://github.com/Myzel394/NumberHub/blob/master/PRIVACY.md",
+                        )
+                    },
+                )
+            }
+
             // OPEN SOURCE
             item {
                 ListItem(
